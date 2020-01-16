a31c39899220498821baa33675cd5e2f7aab4346
stevekutz <stkutz@gmail.com>
2020-01-15 18:32:51 -0600

    sprite inside green Map inside World comp

--- app/src/features/map/index.js
+++ app/src/features/map/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Map = (props) => {
     return (
-        <div>
-        
+        <div
+            style = {{
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                width: '800px',
+                height: '400px',
+                backgroundColor: 'green',
+                border: '4px solid white',
+
+
+                
+            }}
+        >    
         </div>
     )
 }
